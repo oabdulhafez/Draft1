53397e39bbf1b74e3ec84e121808f8408d406e72
guille <gdueasar@hawk.iit.educlearexit()(git config --global user.email gdueasar@hawk.iit.edu>
2017-08-25 14:21:07 -0500

    Fix voxel grid size bug.

--- integrity-master/src/Main/src/classes.cpp
+++ integrity-master/src/Main/src/classes.cpp
@@ -6,9 +6,9 @@
 Parameters::Parameters()
 {
 	numFrames= 5; // number of pcd files to read
-	min_cluster_size= 10; //min # of points of each cluster. If less than this -> remove cluster
+	min_cluster_size= 18; //min # of points of each cluster. If less than this -> remove cluster
 	min_plane_size= 100; // min # of points of each plane. If less than this -> do not extract plane
-	min_density= 500; // min density of points of each cluster. If less dense than this -> remove cluster
+	min_density= 550; // min density of points of each cluster. If less dense than this -> remove cluster
 	min_cluster_distance= 0.8; //1.5 when clustering, this is the minimum distance in between two clusters. If the distance is less than this, they won't be divide into two clusters
 	xlim= 5.51; //6 points with x coordinate farther than this are removed
 	ylim= 5.51; //6 points with y coordinate farther than this are removed
@@ -22,8 +22,8 @@ Parameters::Parameters()
 	plane_normal_distance_weight= 0.03; // RANSAC parameter. See documentation
 	plane_RANSAC_max_iter= 20; // Max # of iterations for the plane extraction. RANSAC parameter.
 	normal_radius_search= 0.1; // Create normals using points closer than this distance
-	min_cylinder_size= 10; // extract cylinders with at least this # of points
-	min_association_distance= 1.5; //2 a cylinder is associated to a landmark in the map if is closer than this distance
+	min_cylinder_size= 18; // extract cylinders with at least this # of points
+	min_association_distance= 1.7; //2 a cylinder is associated to a landmark in the map if is closer than this distance
 	rlim= 5.51; //6 points farther than this distance from the LIDAR are removed
 }
 
